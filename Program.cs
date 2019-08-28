9b235aad10daef140c00e214815c94cb7e9502c5
unknown <Sala300@TUNDFPCGE10F358.FormacionSena.RED>
2019-08-28 16:08:34 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Ejercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Proyector pro = new Proyector();
+            pro.marca = "S39";
+            pro.modelo = "Epson";
+            pro.peso = 2.7;
+            pro.valor = 1379900;
+            Console.WriteLine(pro.marca);
+            Console.WriteLine(pro.modelo);
+            Console.WriteLine(pro.peso);
+            Console.WriteLine(pro.valor);
+            Console.ReadLine();
+            Proyector pro1 = new Proyector("Cinemac", "Mini YG-320");
+            
+            Console.WriteLine(pro1.marca);
+            Console.WriteLine(pro1.modelo);
+          
+            Console.ReadLine();
+            Proyector pro2 = new Proyector(3.5,1200200);
+            Console.WriteLine(pro2.peso);
+            Console.WriteLine(pro2.valor);
+            Console.ReadLine();
+
+
+
+            Console.WriteLine($"La marca es: {pro.marca} ");
+            Console.WriteLine($"El modelo es: {pro.modelo} ");
+            Console.WriteLine($"El peso es: {pro.peso} ");
+            Console.WriteLine($"El valor es: {pro.valor} \n");
+
+            Console.WriteLine($"La marca es: {pro1.marca}");
+            Console.WriteLine($"El modelo es: {pro1.modelo} \n");
+
+            Console.WriteLine($"El peso es: {pro2.peso} ");
+            Console.WriteLine($"El valor es: {pro2.valor} \n");
+
+            pro.IngresarProyector();
+            pro.ImprimirProyector();
+            pro.ModificarMarca("Brightside");
+            pro.ImprimirProyector();
+
+        }
+    }
+}
---
+++ Proyector.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio
+{
+    class Proyector
+    {
+        #region Propiedades
+        public string marca { get; set; }
+        public string modelo { get; set; }
+        public double peso { get; set; }
+        public int valor { get; set; }
+
+        #endregion
+
+        #region Constructor
+        public Proyector()
+        {
+
+        }
+
+        public Proyector(string mar,string mod)
+        {
+            marca = mar;
+            modelo = mod;
+        }
+
+        public Proyector(double pes, int val)
+        {
+            peso = pes;
+            valor = val;
+        }
+        #endregion
+        #region Metodos
+        public void IngresarProyector()
+        {
+            Console.WriteLine("Ingresar la marca del proyector");
+            marca = Console.ReadLine();
+            Console.WriteLine("Ingresar el modelo del proyector");
+            modelo = Console.ReadLine();
+            Console.WriteLine("Ingresar el peso del proyector");
+            peso = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingresar el valor del proyector");
+            valor = int.Parse(Console.ReadLine());
+
+        }
+        public void ImprimirProyector()
+        {
+            Console.WriteLine($"Marca:  {marca}");
+            Console.WriteLine($"Modelo:  {modelo}");
+            Console.WriteLine($"Peso:  {peso}");
+            Console.WriteLine($"Valor:  {valor}");
+        }
+        public void ModificarMarca(string ma)
+        {
+            marca = ma;
+        }
+        #endregion
+    }
+}
